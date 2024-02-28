e3a8a903b50268e601deb4f99082b0be659417e5
Iron-E <code.iron.e@gmail.com>
2024-02-28 14:09:29 -0500

    fix: mark interface implementations with default params as optional

--- src/rust/cache.ts
+++ src/rust/cache.ts
@@ -7,23 +7,37 @@ export type CargoCacheVolumes = Readonly<{
 	registry: CacheVolume,
 }>
 
+/** The default cargo bin volume */
+export const BIN_VOLUME_ID = "cargo-bin" as const;
+
+/** The default cargo Git DB volume*/
+export const GIT_DB_VOLUME_ID = "cargo-git-db" as const;
+
+/** The default cargo registry volume */
+export const REGISTRY_VOLUME_ID = "cargo-registry" as const;
+
 declare module '@dagger.io/dagger' {
 	interface Client {
 		/**
-		 * @param path see {@link Container.withDirectory} and {@link Container.withWorkDir}
-		 * @param directory see {@link Container.withDirectory}
-		 * @param opts see {@link Container.withDirectory}
-		 * @returns the container with the `path` available as the current working `directory`.
+		 * @param binVolume the unique name for the bin volume. Defaults to {@link BIN_VOLUME_ID}
+		 * @param gitDbVolume the unique name for the git db volume. Defaults to {@link GIT_DB_VOLUME_ID}
+		 * @param registryVolume the unique name for the registry volume. Defaults to {@link REGISTRY_VOLUME_ID}
+		 * @returns the cache volumes
 		 */
-		cargoCacheVolumes(this: this, binVolume: string, gitDbVolume: string, registryVolume: string): CargoCacheVolumes;
+		cargoCacheVolumes(
+			this: this,
+			binVolume?: string,
+			gitDbVolume?: string,
+			registryVolume?: string,
+		): CargoCacheVolumes;
 	}
 }
 
 Client.prototype.cargoCacheVolumes = function(
 	this: Client,
-	binVolume: string = "cargo-bin",
-	gitDbVolume: string = "cargo-git-db",
-	registryVolume: string = "cargo-registry",
+	binVolume: string = BIN_VOLUME_ID,
+	gitDbVolume: string = GIT_DB_VOLUME_ID,
+	registryVolume: string = REGISTRY_VOLUME_ID,
 ): CargoCacheVolumes {
 	return {
 		bin: this.cacheVolume(binVolume),
--- src/rust/container.ts
+++ src/rust/container.ts
@@ -4,31 +4,39 @@ import type { CargoCacheVolumes } from './cache';
 /** base dependencies for working with rust projects */
 export const BASE_DEPENDENCIES = ["clang", "file", "gcc", "git", "lld", "musl-dev", "openssl", "openssl-dev"] as const;
 
+export const CARGO_CACHE_MOUNT_POINT = '/usr/local/cargo/.ci_cache' as const;
+
 declare module '@dagger.io/dagger' {
 	interface Container {
 		/**
-		 * @param volumes the cache volumes
+		 * @param volumes the cache volumes.
+		 * @param mountPoint the root dir where the caches are mounted. Defaults to {@link CARGO_CACHE_MOUNT_POINT}
 		 * @returns the container with the cargo cache volumes mounted
 		 * @see {@link Client.cargoCacheVolumes}
 		 * @see {@link Container.withMountedCache}
 		 */
-		withCargoCache(this: Readonly<this>, volumes: CargoCacheVolumes, mountPoint: string): this;
+		withCargoCache(this: Readonly<this>, volumes: CargoCacheVolumes, mountPoint?: string): this;
 
 		/**
 		 * @param crate from {@link crates.io}.
-		 * @param features to enable in the create. Comma separated
-		 * @param defaultFeatures to use default features or not
+		 * @param features to enable in the create. Comma separated. Defaults to no features.
+		 * @param defaultFeatures to use default features or not. Defaults to `true`
 		 * @returns the container with the `path` available as the current working `directory`.
 		 * @see `cargo install --help`
 		 */
-		withCargoInstall(this: Readonly<this>, crate: string, features: string, defaultFeatures: boolean): this;
+		withCargoInstall(
+			this: Readonly<this>,
+			crate: string,
+			features?: readonly string[],
+			defaultFeatures?: boolean,
+		): this;
 	}
 }
 
 Container.prototype.withCargoCache = function(
 	this: Container,
 	volumes: CargoCacheVolumes,
-	mountPoint: string = '/usr/local/cargo/.ci_cache',
+	mountPoint: string = CARGO_CACHE_MOUNT_POINT,
 ): Container {
 	return this
 		.withEnvVariable('CARGO_HOME', mountPoint)
@@ -40,10 +48,10 @@ Container.prototype.withCargoCache = function(
 Container.prototype.withCargoInstall = function(
 	this: Container,
 	crate: string,
-	features: string = "",
+	features: readonly string[] = [],
 	defaultFeatures: boolean = true,
 ): Container {
-	const installArgs = ['cargo', 'install', '--force', crate, '--features', features];
+	const installArgs = ['cargo', 'install', '--force', crate, '--features', features.join(',')];
 	if (!defaultFeatures) {
 		installArgs.push('--no-default-features');
 	}
