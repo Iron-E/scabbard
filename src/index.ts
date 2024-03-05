4a8c0fab6450b3cd85fc2ba31a7b658d6d6b59bd
Iron-E <code.iron.e@gmail.com>
2024-03-05 11:33:45 -0500

    fix(index): export `derive` from` pipelines`

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ export type * from './fs';
 export type * from './pipelines';
 export type * from './util';
 
-export { declare, enqueue, inject, run } from './pipelines';
+export { declare, derive, enqueue, run } from './pipelines';
 export { parseIgnoreFile as readIgnoreFile } from './fs';
