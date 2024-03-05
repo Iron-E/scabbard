9bfd48fd06adb1f52fbf6dcdf1261b0689907086
Iron-E <code.iron.e@gmail.com>
2024-03-04 21:07:13 -0500

    ref: export `update` and `inject` from `pipelines`

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ export type * from './fs';
 export type * from './pipelines';
 export type * from './util';
 
-export { enqueue, run } from './pipelines';
+export { declare, enqueue, inject, run } from './pipelines';
 export { parseIgnoreFile as readIgnoreFile } from './fs';
