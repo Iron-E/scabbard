7c3e3e49071c3fdefdc1ee58cacda189a4d49298
Iron-E <code.iron.e@gmail.com>
2024-03-05 17:34:49 -0500

    ref(std_scope)!: use new `Scope` features

--- src/rust/scope/client.ts
+++ src/rust/scope/client.ts
@@ -1,11 +1,16 @@
 import '../cache';
 import { BASE_DEPENDENCIES } from '../container';
-import { set, setFrom, setTo } from '../../pipelines';
-import { ignoreFile } from '../../std_scope/fs';
 import { HOST_PROJECT_DIR } from '../../std_scope/client';
+import { IGNORE_FILE } from '../../std_scope/fs';
+import { set, setFrom, setTo } from '../../pipelines';
 import { type Client, CacheVolume, Container, Directory } from '@dagger.io/dagger';
 
 const prefix = '__scabbardRustClient' as const;
+
+/**
+ * @returns `string` the base image used for Rust containers
+ */
+export const BASE_IMAGE_NAME = setTo(`${prefix}BaseImageName`, 'rust:alpine');
 
 /**
  * The {@link Client} with {@link Client.cargoHomeCache}
@@ -15,47 +20,48 @@ const prefix = '__scabbardRustClient' as const;
 export const CARGO_CACHE_HOME = set(`${prefix}CargoHomeCache`, client => client.cargoHomeCache());
 
 /**
- * @returns string the base image used for Rust containers
- */
-export const BASE_IMAGE_NAME = setTo(`${prefix}BaseImageName`, 'rust:alpine');
-
-/**
  * @param {@link BASE_IMAGE_NAME} the image name
  * @param {@link CARGO_CACHE_HOME} the cargo cache
  * @returns {@link Container} the {@link Client} with `rust:alpine` as a base image, and with the base dependencies.
  */
-export const baseContainer = setFrom([BASE_IMAGE_NAME, CARGO_CACHE_HOME], `${prefix}base`, (client, inject) =>
-	client
+export const BASE_CONTAINER = setFrom([BASE_IMAGE_NAME, CARGO_CACHE_HOME], `${prefix}base`, async (client, inject) => {
+	const baseImageName = (await inject(BASE_IMAGE_NAME)).type('string');
+	const cargoCacheHome = (await inject(CARGO_CACHE_HOME)).instance(CacheVolume);
+
+	return client
 		.container()
 		.pipeline('install deps')
-		.fromWithDeps(inject(BASE_IMAGE_NAME).type('string'), BASE_DEPENDENCIES)
+		.fromWithDeps(baseImageName, BASE_DEPENDENCIES)
 		.pipeline('mount cargo cache')
-		.withCargoHome(inject(CARGO_CACHE_HOME).instance(CacheVolume)),
-);
+		.withCargoHome(cargoCacheHome)
+		;
+});
 
 /**
- * @param {@link baseContainer} the base container
- * @param {@link ignoreFile} the excluded
+ * @param {@link BASE_CONTAINER} the base container
+ * @param {@link HOST_PROJECT_DIR} the excluded fs entries
+ * @param {@link IGNORE_FILE} the excluded directories
  * @returns {@link Container} the {@link Client} with a bare container having mounted the {@link HOST_PROJECT_DIR}
  */
-export const withProject = setFrom([baseContainer, ignoreFile], `${prefix}base`, (_, inject) =>
-	inject(baseContainer)
-		.instance(Container)
+export const WITH_PROJECT = setFrom([BASE_CONTAINER, HOST_PROJECT_DIR, IGNORE_FILE], `${prefix}base`, async (_, inject) => {
+	const baseContainer = (await inject(BASE_CONTAINER)).instance(Container);
+	const hostProjectDir = (await inject(HOST_PROJECT_DIR)).instance(Directory);
+	const ignoreFile = (await inject(IGNORE_FILE)).optional.check((v): v is string[] => {
+		return v instanceof Array && typeof v[0] === 'string';
+	});
+
+	return baseContainer
 		.pipeline('copy project directory')
-		.withWorkDirectory(inject(HOST_PROJECT_DIR).instance(Directory), {
-			exclude: inject(ignoreFile).optional.check((v): v is string[] => {
-				return v instanceof Array && typeof v[0] === 'string';
-			})
-		})
-);
+		.withWorkDirectory(hostProjectDir, { exclude: ignoreFile });
+});
 
 /**
- * @param {@link withProject} the container to install cargo hack on
- * @returns {@link Container} the {@link withProject | project container} with `cargo hack`
+ * @param {@link WITH_PROJECT} the container to install cargo hack on
+ * @returns {@link Container} the {@link WITH_PROJECT | project container} with `cargo hack`
  */
-export const withCargoHack = setFrom([withProject], `${prefix}CargoHack`, (_, inject) =>
-	inject(withProject)
-		.instance(Container)
+export const WITH_CARGO_HACK = setFrom([WITH_PROJECT], `${prefix}CargoHack`, async (_, inject) => {
+	const withProject = (await inject(WITH_PROJECT)).instance(Container);
+	return withProject
 		.pipeline('install cargo-hack')
-		.withCargoInstall('cargo-hack@0.6.20')
-);
+		.withCargoInstall('cargo-hack@0.6.20');
+});
--- src/std_scope/client.ts
+++ src/std_scope/client.ts
@@ -5,16 +5,15 @@ const prefix = '__scabbardClient' as const;
 
 /**
  * The path on the host to the source code to mount
+ * @returns `string`
  */
 export const PROJECT_PATH = setTo(`${prefix}ProjectPath`, '.');
 
 /**
- * @param {@link PROJECT_PATH} `string`
+ * @param {@link PROJECT_PATH}
  * @returns {@link Directory} the project path as a directory
  */
-export const HOST_PROJECT_DIR = setFrom([PROJECT_PATH], `${prefix}Project`, (client, inject) =>
-	client
-		.host()
-		.directory(inject(PROJECT_PATH).type('string'))
-	,
-);
+export const HOST_PROJECT_DIR = setFrom([PROJECT_PATH], `${prefix}Project`, async (client, inject) => {
+	const projectPath = (await inject(PROJECT_PATH)).type('string');
+	return client.host().directory(projectPath);
+});
